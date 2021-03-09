7f869eaff54858f335d663d9df1ce2d91efe6ef8
kimstars <buikika9@gmail.com>
2021-03-09 21:57:47 +0700

    Update KiemTra.java

--- KiemTra.java
+++ KiemTra.java
@@ -5,15 +5,13 @@ public class KiemTra {
 
     
     public static void main(String[] args) {
-        HinhTron htron;
-        htron = new HinhTron();
-        htron.setBk((float) 1);
+        HinhTron htron = new HinhTron();
+        htron.setBk(1);
         System.out.println("Dien tich hinh tron la: "+htron.dt());
         System.out.println("Xau ky tu: "+ htron.toString());
         
         System.out.println("----------------------------------------------");
-        HinhTru htru;
-        htru = new HinhTru();
+        HinhTru htru = new HinhTru();
         htru.setCao(1);
         System.out.println("The tich hinh tru voi ban kinh "+htron.getBk()+" la: "+htru.thetich());
         System.out.println("Xau ky tu: "+ htru.toString());
